f83f7c3e9fd4aab1d3719d4ad73926aef2a21419
knavesx <canates051@icloud.com>
2024-09-12 19:57:06 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -1,5 +1,13 @@
 export default async (req, res) => {
   const data = [
+  {
+      id: 1,
+      image: "https://media.discordapp.net/attachments/1282140026200653825/1282140058672955402/DcWWWF00TXaI3JkILQU3-Q.png?ex=66e4343a&is=66e2e2ba&hm=be560b5886098e57d2d0c53a71c98f8784e800714c17d1b4935e204241d7a814&=&format=webp&quality=lossless&width=671&height=671",
+      name: "Knaves",
+      description: "",
+      link: "(https://discord.com/users/987087764661088306)",
+    },
+    
     {
       id: 2,
       image: "https://cdn.discordapp.com/attachments/1283805443512205363/1283832833256853556/san.png?ex=66e46dff&is=66e31c7f&hm=ed9d2e1b69a19454d822c0ec1eeddfaab4201b6cc4c2ede29dbb57c66f799c21&",
