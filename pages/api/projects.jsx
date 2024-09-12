adbcb1f152dcee9944efaa419f5a8952e3856e4b
knavesx <canates051@icloud.com>
2024-09-12 19:49:40 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -4,7 +4,7 @@ export default async (req, res) => {
       id: 2,
       image: "https://media.discordapp.net/attachments/1282140026200653825/1282140141925699667/O_0NhV1AS3mQVwIwKAEtBQ.png?ex=66e4344e&is=66e2e2ce&hm=3b5437e576779fe8cb5c7f9cadc868c0e065d30dd15855c9d7b7a167cf699f1e&=&format=webp&quality=lossless&width=671&height=671",
       name: "Discord",
-      description: "I am a professional editor and software developer. I have been professionally involved in community management and development for 2 years. I have high-level knowledge and experience in areas such as community management and social media management.",
+      description: "",
       link: "https://discord.gg/qeW6Gjz3Zd",
     }
   ];
